7954559af3e54fcb6ec7cae114cf4894113c4b16
Michael Ramos <mdramos8@gmail.com>
2025-05-12 15:27:59 -0700

    only payments agent this branch

---
+++ app/banking-assistant/Agent/PaymentAgent.cs
@@ -0,0 +1,93 @@
+﻿/// <summary>
+/// Represents an agent responsible for handling payment-related operations.
+/// </summary>
+public class PaymentAgent : IPaymentAgent
+{
+    private ChatCompletionAgent? _agent; // Marked as nullable
+    private ILogger<PaymentAgent> _logger;
+    private readonly IUserService _userService;
+    private readonly IConfiguration _configuration;
+    private readonly Kernel _kernel;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PaymentAgent"/> class.
+    /// </summary>
+    /// <param name="kernel">The kernel instance for managing plugins and functions.</param>
+    /// <param name="configuration">The application configuration.</param>
+    /// <param name="documentScanner">The document scanner for scanning invoices.</param>
+    /// <param name="userService">The user service for retrieving logged-in user information.</param>
+    /// <param name="logger">The logger instance for logging operations.</param>
+    public PaymentAgent(Kernel kernel, IConfiguration configuration, IDocumentScanner documentScanner, IUserService userService, ILogger<PaymentAgent> logger)
+    {
+        _logger = logger;
+        _userService = userService;
+        _configuration = configuration;
+        _kernel = kernel.Clone();
+    }
+
+    /// <summary>
+    /// Gets the <see cref="ChatCompletionAgent"/> instance, creating it if it does not already exist.
+    /// </summary>
+    public ChatCompletionAgent Agent
+    {
+        get
+        {
+            if (_agent == null)
+            {
+                _agent = CreateAgentAsync().GetAwaiter().GetResult();
+            }
+            return _agent;
+        }
+    }
+
+    /// <summary>
+    /// Asynchronously creates a new <see cref="ChatCompletionAgent"/> instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the created <see cref="ChatCompletionAgent"/>.</returns>
+    private async Task<ChatCompletionAgent> CreateAgentAsync()
+    {
+
+        var paymentTools = await AgenticUtils.AddMcpServerPluginAsync(
+            clientName: "banking-assistant-client",
+            pluginName: "PaymentsPlugins",
+            apiUrl: _configuration["BackendAPIs:PaymentsApiUrl"] + "/mcp",
+            useStreamableHttp: true
+        );
+
+        var accountTools = await AgenticUtils.AddMcpServerPluginAsync(
+            clientName: "banking-assistant-client",
+            pluginName: "AccountPlugins",
+            apiUrl: _configuration["BackendAPIs:AccountsApiUrl"] + "/mcp",
+            useStreamableHttp: true
+        );
+
+        _kernel.Plugins.AddFromFunctions("PaymentsPlugins", paymentTools.Select(mcpTools => mcpTools.AsKernelFunction()));
+        _kernel.Plugins.AddFromFunctions("AccountPlugins", accountTools.Select(mcpTools => mcpTools.AsKernelFunction()));
+
+        AgenticUtils.AddOpenAPIPlugin(
+           kernel: _kernel,
+           pluginName: "TransactionHistoryPlugin",
+           apiName: "transaction-history",
+           apiUrl: _configuration["BackendAPIs:TransactionsApiUrl"]
+        );
+
+        _kernel.ImportPluginFromType<InvoiceScanPlugin>(nameof(InvoiceScanPlugin));
+
+        // Special call out of RetainArugumentTypes. The Payments plug takes object as input instead of string.
+        var executionSettigs = new AzureOpenAIPromptExecutionSettings()
+        {
+            FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(options: new() { RetainArgumentTypes = true })
+        };
+
+        return new()
+        {
+            Name = "PaymentAgent",
+            Instructions = String.Format(AgentInstructions.PaymentAgentInstructions, _userService.GetLoggedUser()),
+            Kernel = _kernel,
+            Arguments =
+            new KernelArguments(
+                executionSettings: executionSettigs
+            )
+        };
+    }
+}
---
+++ app/banking-assistant/Agent/Plugins/InvoiceScanPlugin.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+public class InvoiceScanPlugin
+{
+    private readonly ILogger<InvoiceScanPlugin> _logger;
+    private IDocumentScanner _documentScanner;
+    public InvoiceScanPlugin(IDocumentScanner documentScanner, ILogger<InvoiceScanPlugin> logger)
+    {
+        _documentScanner = documentScanner;
+        _logger = logger;
+    }
+    [KernelFunction("scanInvoice")]
+    [Description("Extract the invoice or bill data scanning a photo or image")]
+    public async Task<string> ScanInvoice([DescriptionAttribute("the path to the file containing the image or photo")] string filePath) {
+
+        Dictionary<string, string> scanData = null;
+        _logger.LogInformation($"Attempting to scan: {filePath}");
+
+        try{
+            scanData = await _documentScanner.Scan(filePath);
+        } catch (Exception e) {
+          _logger.LogError($"Error extracting data from invoice {filePath}: {e.ToString()}");
+            scanData = new();
+        }
+
+        _logger.LogInformation($"SK scanInvoice plugin: Data extracted {filePath}:{scanData}");
+        return JsonSerializer.Serialize(scanData);
+    }
+}
+
